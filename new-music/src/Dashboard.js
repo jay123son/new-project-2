d613f238bef03a92078f045641c59de64c09e1fd
jay123son <youremail@domain.com>
2022-11-18 00:57:53 -0500

    added search function

--- new-music/src/Dashboard.js
+++ new-music/src/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import useAuth from './useAuth'
+import { Container, From } from 'react-bootstrap'
 
 export default function Dashboard({code}) {
     const accessToken = useAuth(code)
-  return (
-    <div>{code}</div>
-  )
+  return <Container>
+    <From.Control Type="search" placeholder="Search Songs/Artists" value={search} onChange={e => setSearch(e.target.value)} />
+    </Container>
+  
 }
