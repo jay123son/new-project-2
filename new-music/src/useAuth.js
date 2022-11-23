a79a3a01cd08ec72ea99c6200af4f3b35a24d869
jay123son <youremail@domain.com>
2022-11-22 19:36:11 -0500

    uth

--- new-music/src/useAuth.js
+++ new-music/src/useAuth.js
@@ -10,7 +10,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-    .post("https://new-project-2.vercel.app:3001/refresh", {
+    .post("https://new-project-2-jhdy.vercel.app/refresh", {
         code,
       })
       .then(res => {
@@ -29,7 +29,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-      .post("https://new-project-2.vercel.app:3001/refresh", {
+      .post("https://new-project-2-jhdy.vercel.app/refresh", {
           refreshToken,
         })
         .then(res => {
